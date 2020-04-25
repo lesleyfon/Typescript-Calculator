13080f37e1ed471cb38299cbeb738dd446de973c
lesley <lesleybanadzem2017@gmail.com>
2020-04-25 12:38:45 -0700

    Displaying number on the dom after click of sign

--- app/dashBoard.ts
+++ app/dashBoard.ts
@@ -1,14 +1,18 @@
 export class DashBoard {
-    displayResult: string = ''
+    total: number = 0;
     totalInputValues: object = {}
     inputField: HTMLInputElement = <HTMLInputElement>document.querySelector('input');
+    displayField: HTMLElement = <HTMLElement>document.querySelector('.display')
+
 
     captureNumber(elem: HTMLElement) {
+
         if (["*", "/", "+", "-"].includes(this.inputField.value)) {
             this.totalInputValues = {
                 ...this.totalInputValues,
-                [this.inputField.value +  String(Object.keys(this.totalInputValues).length)] : this.inputField.value
+                [this.inputField.value + String(Object.keys(this.totalInputValues).length)]: this.inputField.value
             }
+          
             this.inputField.value = ''
         }
         this.inputField.value += elem.innerText;
@@ -19,34 +23,37 @@ export class DashBoard {
             ...this.totalInputValues,
             [this.inputField.value + "obj" + String(Object.keys(this.totalInputValues).length)]: this.inputField.value
         }
+        this.displayField.innerText = Object.values(this.totalInputValues)[Object.values(this.totalInputValues).length - 1];
 
-        if(elem.innerText === "=") {
-            console.log(this.totalInputValues)
-            return this.calculateTotal(this.totalInputValues);
+        if (elem.innerText === "=") {
+            this.total = this.calculateTotal(this.totalInputValues);
+            console.log(this.total)
+            return ''
         }
 
         this.inputField.value = elem.innerText;
     }
 
-    calculateTotal(operationObj:object) : void{
+    calculateTotal(operationObj: object): number {
         let arr: string[] = Object.values(operationObj);
-        
-        let total:number = 0;
 
-        for(let i = 0; i < arr.length; i++){
 
-            if (["*", "/", "+", "-"].includes(arr[i])){
+
+        for (let i = 0; i < arr.length; i++) {
+
+            if (["*", "/", "+", "-"].includes(arr[i])) {
                 //is the current number is a sign we call the operation method that accept current number, the next number after the sign and the sign
-                total = this.operation(total, parseFloat(arr[i + 1]), arr[i]);
+                this.total = this.operation(this.total, parseFloat(arr[i + 1]), arr[i]);
                 // after this operation remove the sign number at i + 1
-                arr.splice( i + 1, 1);
-            }else{
-                total = parseFloat(arr[i])
+                arr.splice(i + 1, 1);
+            } else {
+                this.total = parseFloat(arr[i])
             }
         }
+        return this.total
     }
 
-    operation( prev: number, curr:number, operator: string) : number {
+    operation(prev: number, curr: number, operator: string): number {
         switch (operator) {
             case "+":
                 prev = prev + curr;
@@ -64,7 +71,7 @@ export class DashBoard {
                 break;
         }
         return prev;
-        
+
     }
 
 
