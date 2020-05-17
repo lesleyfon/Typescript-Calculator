8f5b96ba74ebbabf4393c1468b3b23a138627253
lesley <lesleybanadzem2017@gmail.com>
2020-05-17 11:34:28 -0700

    Refactored code to use an array for the totalvalues instead on an object

--- app/dashBoard.ts
+++ app/dashBoard.ts
@@ -1,48 +1,46 @@
 export class DashBoard {
     total: number = 0;
-    totalInputValues: object = {}
+    totalInputValues: string[] = []
     inputField: HTMLInputElement = <HTMLInputElement>document.querySelector('input');
     displayField: HTMLElement = <HTMLElement>document.querySelector('.display')
 
 
     captureNumber(elem: HTMLElement) {
-
-        if (["*", "/", "+", "-"].includes(this.inputField.value)) {
-            console.log(this.inputField.value)
-            this.totalInputValues = {
-                ...this.totalInputValues,
-                [this.inputField.value + String(Object.keys(this.totalInputValues).length)]: this.inputField.value
-            }
-          
-            this.inputField.value = ''
+        if (["*", "/", "+", "-", "="].includes(this.inputField.value)) {
+            this.inputField.value = "";
         }
-
         this.inputField.value += elem.innerText;
     }
 
     captureSign(elem: HTMLElement) {
-        this.totalInputValues = {
-            ...this.totalInputValues,
-            [this.inputField.value + "obj" + String(Object.keys(this.totalInputValues).length)]: this.inputField.value
+
+        this.displayField.innerText = this.inputField.value
+
+        if (["*", "/", "+", "-", "="].includes(elem.innerText)) {
+            this.totalInputValues.push(this.inputField.value)
+            this.totalInputValues.push(elem.innerText)
+            this.inputField.value = elem.innerText
         }
-        this.displayField.innerText = Object.values(this.totalInputValues)[Object.values(this.totalInputValues).length - 1];
 
         if (elem.innerText === "=") {
+
             this.total = this.calculateTotal(this.totalInputValues);
             this.inputField.value = ''
             this.displayField.innerText = String(this.total);
-            this.totalInputValues = {
-                [this.total + 'obj'] : String(this.total)
-            }
-            return 
+            // this.totalInputValues = {
+            //     [this.total + 'obj']: String(this.total);
+            // }
+            this.totalInputValues = []
+            return
         }
 
         this.inputField.value = elem.innerText;
     }
 
-    calculateTotal(operationObj: object): number {
-        let arr: string[] = Object.values(operationObj);
-
+    calculateTotal(values: string[]): number {
+        let arr: string[] = values;
+        const equalSign = arr.splice((arr.length-1), 1)
+        console.log(arr)
         for (let i = 0; i < arr.length; i++) {
             if (["*", "/", "+", "-"].includes(arr[i])) {
                 //is the current number is a sign we call the operation method that accept current number, the next number after the sign and the sign
