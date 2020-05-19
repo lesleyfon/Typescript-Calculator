9e10687fd514b97cbcd1758dcf7c67455b1d5c95
lesley <lesleybanadzem2017@gmail.com>
2020-05-18 23:36:43 -0700

    Minor refactors and code structuring

--- app/dashBoard.ts
+++ app/dashBoard.ts
@@ -17,21 +17,21 @@ export class DashBoard {
         this.displayField.innerText = this.inputField.value
 
         if (["*", "/", "+", "-", "="].includes(elem.innerText)) {
-            this.totalInputValues.push(this.inputField.value)
-            this.totalInputValues.push(elem.innerText)
+
+            this.totalInputValues.push(this.inputField.value);
+            this.totalInputValues.push(elem.innerText);
+
+            this.totalInputValues[this.totalInputValues.length - 1] = elem.innerText
             this.inputField.value = elem.innerText
         }
 
         if (elem.innerText === "=") {
-
             this.total = this.calculateTotal(this.totalInputValues);
             this.inputField.value = ''
             this.displayField.innerText = String(this.total);
-            // this.totalInputValues = {
-            //     [this.total + 'obj']: String(this.total);
-            // }
-            this.totalInputValues = []
-            return
+
+            this.totalInputValues = [String(this.total)];
+        
         }
 
         this.inputField.value = elem.innerText;
@@ -39,8 +39,8 @@ export class DashBoard {
 
     calculateTotal(values: string[]): number {
         let arr: string[] = values;
-        const equalSign = arr.splice((arr.length-1), 1)
-        console.log(arr)
+        const equalSign = arr.splice((arr.length - 1), 1)
+
         for (let i = 0; i < arr.length; i++) {
             if (["*", "/", "+", "-"].includes(arr[i])) {
                 //is the current number is a sign we call the operation method that accept current number, the next number after the sign and the sign
@@ -48,7 +48,7 @@ export class DashBoard {
                 // after this operation remove the sign number at i + 1
                 arr.splice(i + 1, 1);
             } else {
-                this.total = parseFloat(arr[i])
+                this.total = Number(arr[i])
             }
         }
         return this.total
@@ -69,9 +69,9 @@ export class DashBoard {
                 prev = prev / curr;
                 return prev;
             default:
-                break;
+                return prev;
         }
-        return prev;
+        // return prev;
 
     }
 
